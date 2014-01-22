3352f86e86c3b8e0f9f5c41832af6a75d671981a
Chase Coney <chasiepoo@gmail.com>
2014-01-22 16:44:49 -0600

    Added argparse

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python
 
 import re
+import argparse
 
-#letters = raw_input("Enter your letters: ");
-letters = 'vtlarxrz'
-
-# Format regex for 0-1 uses of each letter
+# String to hold regex
 regex = ''
-for char in letters:
-  regex += char + '?'
-
-m = re.compile('^' + regex.lower() + '$')
-
-# Read in word list
-words = [line.strip() for line in open('enable1.txt')]
 
 # Create a storage area for sorting words by size
 sortedDict = {}
 
+# Setup argument parsing
+parser = argparse.ArgumentParser(description='Generate list of valid Words With Friends words from given letters.')
+
+parser.add_argument("letters", help="List of letters to lookup words by.")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+args = parser.parse_args()
+
+regex = '^[' + args.letters.lower() + ']+$'
+
+m = re.compile(regex)
+
+# Read in word list
+words = [line.strip() for line in open('enable1.txt')]
+
 # Find words that match the letters given
 for word in words:
   if (m.match(word) is not None):
-    wordLen = word.__len__()
+    wordLen = len(word)
     # Make sure the key of dict exists
     if wordLen not in sortedDict:
       sortedDict[wordLen] = []
@@ -29,8 +35,15 @@ for word in words:
     # Add item to list
     sortedDict[wordLen].append(word)
 
+# If verbose output is enabled
+if args.verbose:
+  print 'Your letters: ' + args.letters
+  print 'Regular expression used: ' + regex
+  print '# of words searched: ' + str(len(words))
+
 # Print words by size
 for wordLength, words in sortedDict.iteritems():
   print wordLength
   for word in words:
     print ' ' + word
+
